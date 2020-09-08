53c0b3bc137d29139f6aae39132d30a9d7980615
oggep <54796870+oggep@users.noreply.github.com>
2020-09-08 10:29:49 +0200

    Hej kompis

--- Webb/app.js
+++ Webb/app.js
@@ -1,1 +1,7 @@
-Hello Niklas [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile(__dirname + "\\Client\\index.html"))
+app.get('/korv', (req, res) => res.sendFile(__dirname + "\\Client\\korv.css"))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
