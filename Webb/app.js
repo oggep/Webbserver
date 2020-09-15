52f6ecb3ac44a7a9ee16051f255cbbb2903823fc
oggep <54796870+oggep@users.noreply.github.com>
2020-09-15 10:26:12 +0200

    Grejor

--- Webb/app.js
+++ Webb/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => res.sendFile(__dirname + "\\Client\\index.html"))
 app.get('/korv', (req, res) => res.sendFile(__dirname + "\\Client\\korv.css"))
+
+app.post('/', (req, res) => {
+    res.json(req.body);
+    console.log(req.body["uname"] +  " " +  req.body["email"]);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
