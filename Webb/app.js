74e055ef17d38bccf7674a3fb336833c72fc25bf
oggep <54796870+oggep@users.noreply.github.com>
2020-10-16 14:10:03 +0200

    gg

--- Webb/app.js
+++ Webb/app.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Webshop', {useNewUrlParser: true});
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => res.sendFile(__dirname + "\\Client\\index.html"))
 app.get('/korv', (req, res) => res.sendFile(__dirname + "\\Client\\korv.css"))
 
+const userSchema = new mongoose.Schema({
+  unamne: String,
+  email: String,
+});
+
+const User = mongoose.model('User', usernSchema);
+
+
 app.post('/', (req, res) => {
     res.json(req.body);
-    console.log(req.body["uname"] +  " " +  req.body["email"]);
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+user.save();
